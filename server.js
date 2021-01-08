a7f236f2c64ec1802c6e7e2ae0f26650f64c3bdb
dimplast <dimplast@gmail.com>
2021-01-08 16:34:10 +0200

    ‘myapi’

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const  bootcamps  = require('./routes/bootcamps');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
@@ -15,11 +16,13 @@ if(process.env.NODE_ENV === 'development'){
 
 connectDB();
 
+app.use(express.json());
+
 app.use('/api/v1/bootcamps',bootcamps);
 
 const server = app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} 
-    mode on port ${PORT}`));
+    mode on port ${PORT}`.yellow.bold));
 
 process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error:${err.message}`);
